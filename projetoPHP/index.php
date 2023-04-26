39d442081e8ecc84f1111d77e440f156889d6c68
Rafael Filimberti <rfilimberti@gmail.com>
2023-04-25 21:41:45 -0300

    Envio arquivo PHP

---
+++ projetoPHP/index.php
@@ -0,0 +1,52 @@
+<?php
+
+//criando varável no PHP
+// variavel sempre se cria com um cifrão (dolar) antes de qualquer nome de variavel no PHP
+
+$nomeDaVariavel;
+
+$nome = "";
+
+// toda e qualquer instrução PHP vai o ponto e virgula no final ;
+
+
+//comentários no PHP
+
+//comentários em uma linha
+
+/* comentario
+ em 
+bloco
+*/
+
+/**
+ * comentario em bloco
+ * de intrução de
+ * documentacao de codigo
+ * @variable int $idade
+ */
+
+ //funções em PHP
+ function nomeDaFuncao() {}
+
+    // Funções com parametro
+    function nomeDaFuncaoComParametros($n1, $n2, $n3) {}
+
+    //mostrar mensagem na tela
+    echo "minha mensagem";
+    print_r("minha mensagem no print");
+ 
+
+    //operações
+    /*
+    + = Adição
+    - = subtração
+    * = multiplicação
+    / = divisão
+    % = MOD (resto da divisão) ($num % 2)
+    */
+
+
+
+    
+?>
